59468859ca54091ec59eddbc5aa0b7788fc2ebea
jyo33 <21pa1a0465@vishnu.edu.in>
2024-05-28 10:55:42 +0530

    first commit

---
+++ chatbot.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6020980438:AAFDJCBa2ribA9xpOVALiMOhQuXrWUjnyzY';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', function(mg){
+request('http://www.omdbapi.com/?t='+mg.text+'&apikey=ea77e202', function (error, response, body) {
+  if(JSON.parse(body).Response=="True"){
+    bot.sendMessage(mg.chat.id, "Title "+JSON.parse(body).Title)
+    bot.sendMessage(mg.chat.id, "Release Date "+JSON.parse(body).Released)
+    bot.sendMessage(mg.chat.id, "Actors "+JSON.parse(body).Actors)
+    bot.sendMessage(mg.chat.id, "Rating "+JSON.parse(body).Ratings[0].Value)
+  }
+  else{
+      bot.sendMessage(mg.chat.id, "Movie not found")
+  }
+});
+})
